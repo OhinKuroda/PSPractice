a8a68ada34017c7ae3136338eeede1f48b74f324
Ohin Carter <ohin.t.carter@outlook.com>
2023-07-25 08:49:29 +0900

    testing arrays

---
+++ Array&HashPractice.ps1
@@ -0,0 +1,17 @@
+$h1 = @{
+    Doors = 4
+    Wheels = 4
+    EngineType = 'Gas'
+}
+
+$h2 = @{
+    Doors = 4
+    Wheels = 4
+    EngineType = 'Electric'
+}
+
+[System.Collections.ArrayList]$Cars = @()
+
+$Cars.add($h2)
+
+$Cars
