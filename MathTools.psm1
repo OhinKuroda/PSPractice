ee1b7d56f95ea39d7b2c12323527e42775ccfc47
Ohin Carter <ohin.t.carter@outlook.com>
2023-07-24 23:44:35 -0700

    added help info and Cmdletbindings to mathtools

--- MathTools.psm1
+++ MathTools.psm1
@@ -2,23 +2,129 @@
 #must never call function in a module
 #one funtion for adding and one for multiplying
 
+#psm1 means that its maid to be a module
+#must never call function in a module
+#one funtion for adding and one for multiplying
+
 function Get-Addition {
+    <#
+    .SYNOPSIS
+        This command adds two numbers together'
+    .DESCRIPTION
+        This command takes two inputs as parameters and then adds them together. 
+        It produces a PS object showing the two numbers and the result
+    .NOTES
+        Created by Ohin Carter
+        ChangeLog:
+        Date                   Version              What was changed
+        ----                   --------             ----------------
+        24/Jul/2023             1.0.0               Script was created
+        24/Jul/2023             1.0.5               added division
+        24/Jul/2023             1.1.0               added help info and cmdletbinding
+    
+    .LINK
+        Specify a URI to a help page, this will show when Get-Help -Online is used.
+    .EXAMPLE
+       Get-addition -num1 10 -num2 20
+
+    .PARAMETER Num1
+        This is the first number
+
+    .PARAMETER Num2
+        This is the second
+    #>
+    [CmdletBinding()]
     param (
         $Num1,
         $Num2     
     )
-    return $Num1 + $Num2
+    $AddHash = [ordered]@{
+        Num1 = $Num1
+        Num2 = $Num2
+        Result = $Num1 + $Num2
+
+    }
+    $ResultObj = New-Object -TypeName psobject -Property $AddHash
+    return $ResultObj
 }
 
 function Get-Multiplication {
+    <#
+   .SYNOPSIS
+        This command multiplies two numbers together'
+    .DESCRIPTION
+        This command takes two inputs as parameters and then multiplies them together. 
+        It produces a PS object showing the two numbers and the result
+    .NOTES
+        Created by Ohin Carter
+        ChangeLog:
+        Date                   Version              What was changed
+        ----                   --------             ----------------
+        24/Jul/2023             1.0.0               Script was created
+        24/Jul/2023             1.0.5               added division
+        24/Jul/2023             1.1.0               added help info and cmdletbinding
+    
+    .LINK
+        Specify a URI to a help page, this will show when Get-Help -Online is used.
+    .EXAMPLE
+       Get-multiplication -num1 10 -num2 20
+
+    .PARAMETER Num1
+        This is the first number
+
+    .PARAMETER Num2
+        This is the second number
+    #>
+    
+    
+    [CmdletBinding()]
     param (
         $Num1,
         $Num2
     )
-    return $num1 * $num2
+    
+    $MultHash = [Ordered]@{
+        Num1 = $Num1
+        Num2 = $Num2
+        Result = $Num1 * $Num2
+
+    }
+    $ResultObj = New-Object -TypeName psobject -Property $MultHash
+    return $ResultObj
+    
 }
 
+
 function Get-Division {
+    <#
+.SYNOPSIS
+        This command divides two numbers against eachother'
+    .DESCRIPTION
+        This command takes two inputs as parameters and then divides them. 
+        It produces a PS object showing the two numbers and the result
+    .NOTES
+        Created by Ohin Carter
+        ChangeLog:
+        Date                   Version              What was changed
+        ----                   --------             ----------------
+        24/Jul/2023             1.0.0               Script was created
+        24/Jul/2023             1.0.5               added division
+        24/Jul/2023             1.1.0               added help info and cmdletbinding
+    
+    .LINK
+        Specify a URI to a help page, this will show when Get-Help -Online is used.
+    .EXAMPLE
+       Get-division -num1 10 -num2 20
+
+    .PARAMETER Num1
+        This is the first number
+
+    .PARAMETER Num2
+        This is the second number
+    #>
+    
+    
+    [CmdletBinding()]
     param (
         $Num1,
         $Num2
