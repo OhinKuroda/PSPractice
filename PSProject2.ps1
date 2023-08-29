c1fa84836b79de292d6fa2328f40e3139cd7769f
Ohin Carter <ohin.t.carter@outlook.com>
2023-08-28 18:05:18 -0700

    Working well

--- PSProject2.ps1
+++ PSProject2.ps1
@@ -1,27 +1,32 @@
 #prerequisite before starting project
 #Deletes 10 random users
-Invoke-Command -ComputerName LON-DC1 -ScriptBlock {  
-    Get-ADUser -Filter * -Properties Department | Where-Object {$_.Department -in @('Sales','Mareting','Managers')} | Get-Random -Count 10 | Remove-ADUser -Confirm:$false
-    }  
+#Invoke-Command -ComputerName LON-DC1 -ScriptBlock {  
+#    Get-ADUser -Filter * -Properties Department | Where-Object {$_.Department -in @('Sales','Mareting','Managers')} | Get-Random -Count 10 | Remove-ADUser -Confirm:$false
+#    }  
 
 #Install Recycle bin feature in AD
-Invoke-Command -ComputerName LON-DC1 -ScriptBlock {
-        $RecycleBin = Get-ADOptionalFeature 'Recycle Bin Feature'
-        if ($RecycleBin.EnabledScopes.Count -eq 0) {
-            Enable-ADOptionalFeature 'Recycle Bin Feature' -Scope ForestOrConfigurationSet -Target adatum.com -confirm:$false
-              }
-     }  
+#Invoke-Command -ComputerName LON-DC1 -ScriptBlock {
+#        $RecycleBin = Get-ADOptionalFeature 'Recycle Bin Feature'
+#        if ($RecycleBin.EnabledScopes.Count -eq 0) {
+#            Enable-ADOptionalFeature 'Recycle Bin Feature' -Scope ForestOrConfigurationSet -Target adatum.com -confirm:$false
+#               }
+#      }  
       
-# This command finds deleted objects
-Get-ADObject -LDAPFilter:"(msDS-LastKnownRDN=*)" -IncludeDeletedObjects | Where-Object {$_.Deleted -eq $true}
+# # This command finds deleted objects
+# Get-ADObject -LDAPFilter:"(msDS-LastKnownRDN=*)" -IncludeDeletedObjects | Where-Object {$_.Deleted -eq $true}
 
-# This command restores a deleted object 
-Restore-ADObject 
+# # This command restores a deleted object 
+# Restore-ADObject 
 
-# This command can help produce a menu of choices
-Out-Gridview
+# # This command can help produce a menu of choices
+# Out-Gridview
 
-# This command finds deleted objects
+
+
+function RestoreDeletedADUsers {
+  param ()
+  
+  # This command finds deleted objects
 $deletedUsers = Get-ADObject -LDAPFilter:"(msDS-LastKnownRDN=*)" -IncludeDeletedObjects | Where-Object {$_.Deleted -eq $true}
 
 # Create a menu of choices
@@ -30,6 +35,7 @@ foreach ($user in $deletedUsers) {
   $menuItem = New-Object PSObject -Property @{
       Name = $user.Name
       SamAccountName = $user.SamAccountName
+      ObjectGUID = $user.ObjectGUID
   }
   $menuItems += $menuItem
 }
@@ -37,12 +43,16 @@ foreach ($user in $deletedUsers) {
 $selectedUser = $menuItems | Out-GridView -Title "Select a deleted user to restore" -OutputMode Single
 
 if ($selectedUser) {
-  $confirmRestore = Read-Host "Are you sure you want to restore $($selectedUser.Name)? (Y/N)"
+  $confirmRestore = Read-Host "Are you sure you want to restore $($selectedUser.SamAccountName)? (Y/N)"
   if ($confirmRestore -eq "Y") {
   # This command restores a deleted object
-    Restore-ADObject -Identity $selectedUser.SamAccountName -WhatIf
-    # If you're confident, remove the "-WhatIf" parameter to actually perform the restore
+    Restore-ADObject -Identity $selectedUser.ObjectGUID
+    
   } else {
          Write-Host "Restoration of $($selectedUser.Name) canceled."
       }
 } 
+  
+}
+
+RestoreDeletedADUsers
